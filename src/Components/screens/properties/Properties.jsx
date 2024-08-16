a05d7cec6fb153a7649ddf119b3c9c8789b460ba
Muhammad Ali <ai9877572@gmail.com>
2024-08-16 06:49:00 -0700

    Property Page Complete

--- src/Components/screens/properties/Properties.jsx
+++ src/Components/screens/properties/Properties.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import "./style.css";
 import SearchIcon from "../../../assets/svg/SearchIcon";
-import { Flex, Form, Select } from "antd";
+import { Flex, Form, Select, Row, Col } from "antd";
 import getProperty from "../../../action/getProperty";
 import ProductSlider from "../../ProductSlider/ProductSlider";
 import StarsIcon from "../../../assets/svg/StarsIcon.jsx";
+import Email from '../Contact/svg/Email.jsx';
+import Phone from '../Contact/svg/Phone.jsx';
 
 export default function Properties() {
   const [form] = Form.useForm();
@@ -163,18 +165,108 @@ export default function Properties() {
         <ProductSlider data={property} />
       </div>
 
-      <div className="book-property">
+      <section className='booking-section'>
         <StarsIcon />
-        <h2>Let`s Make it Happen</h2>
-        <p className="pb-4">
-          Ready to take the first step toward your dream property? Fill out the
-          form below, and our real estate wizards will work their magic to find
-          your perfect match. Don`t wait; let`s embark on this exciting journey
-          together.
-        </p>
-
-        {/* Here will be the Booking Form */}
-      </div>
+        <main className="booking">
+          <div className='booking-content'>
+            <h1>Let`s Make it Happen</h1>
+            <p>Ready to take the first step toward your dream property? Fill out the form below, and our real estate wizards will work their magic to find your perfect match. Don`t wait; let`s embark on this exciting journey together.</p>
+          </div>
+          <form className='booking-container'>
+            <Row className='inputs-row'>
+              <Col className='inputs-box'>
+                <h6>First Name</h6>
+                <input type="text" placeholder="Enter First Name" required />
+              </Col>
+              <Col className='inputs-box'>
+                <h6>Last Name</h6>
+                <input type="text" placeholder="Enter Last Name" required />
+              </Col>
+              <Col className='inputs-box'>
+                <h6>Email</h6>
+                <input type="email" placeholder="Enter your Email" required />
+              </Col>
+              <Col className='inputs-box'>
+                <h6>Phone Number</h6>
+                <input type="number" placeholder="Enter Phone Number" required />
+              </Col>
+            </Row>
+            <Row className='inputs-row'>
+              <Col className='inputs-box'>
+                <h6>Preferred Location</h6>
+                <select name="cars" id="cars">
+                  <option value="volvo">Volvo</option>
+                  <option value="saab">Saab</option>
+                  <option value="mercedes">Mercedes</option>
+                  <option value="audi">Audi</option>
+                </select>
+              </Col>
+              <Col className='inputs-box'>
+                <h6>Property Type</h6>
+                <select name="cars" id="cars">
+                  <option value="volvo">Volvo</option>
+                  <option value="saab">Saab</option>
+                  <option value="mercedes">Mercedes</option>
+                  <option value="audi">Audi</option>
+                </select>
+              </Col>
+              <Col className='inputs-box'>
+                <h6>No. of Bathroom</h6>
+                <select name="bath" id="bath">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </Col>
+              <Col className='inputs-box'>
+                <h6>No. of Bedroom</h6>
+                <select name="bed" id="bed">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </Col>
+            </Row>
+            <Row className="budget-row">
+              <Col className='budget-box'>
+                <h6>Budget</h6>
+                <select name="socials" id="socials" >
+                  <option value="facebook">Facebook</option>
+                  <option value="instagram">Instagram</option>
+                  <option value="whatsapp">Whatsapp</option>
+                  <option value="other">Other</option>
+                </select>
+              </Col>
+              <Col className="referal-box">
+                <h6>Preferred Contact Method</h6>
+                <Row className="referal">
+                  <div className="options">
+                    <Phone />
+                    <input type="number" placeholder="Enter Phone Number" required />
+                  </div>
+                  <div className="options">
+                    <Email />
+                    <input type="email" placeholder="Enter Email" required />
+                  </div>
+                </Row>
+              </Col>
+            </Row>
+            <Row className='message-row'>
+              <h6>Message</h6>
+              <textarea name="message" id="message" placeholder='Enter Your Message Here..'></textarea>
+            </Row>
+            <Row className='terms-row'>
+              <Col className='condition-section'>
+                <input type="checkbox" name="true" id="ture" />
+                <label htmlFor="true">I agree with <span> Terms of Use</span> and <span>Privacy Policy</span>.</label>
+              </Col>
+              <button>Send your Message</button>
+            </Row>
+          </form>
+        </main>
+      </section>
     </>
   );
 }
